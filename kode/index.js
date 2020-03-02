fe5c32a54d652b98c3cb8878b4236aa0577db9d8
glorytiger <56641017+glorytiger@users.noreply.github.com>
2020-03-02 15:08:42 +0100

    test med enkel form

---
+++ kode/index.js
@@ -0,0 +1,28 @@
+function createNewProduct(event) {
+    //event.preventDefault();
+    
+alert("EY");
+    
+    // Find an element with attribute "name" as "name". In this case <input name="name" placeholder="Product ..."
+    // and retrives the .value - what the user wrote in the <input>
+    const name = document.querySelector("[name='name']").value;
+    
+    // Object construction shorthand - this is the same as writing
+    //const product = {
+    //    name: name,
+    //    price: price, ...
+    //};
+    // Which again could be written as:
+    //const product = {};
+    //product.name = name;
+    // product.price = price;
+    // etc.
+    const task = {name};
+    
+    // Get the STRING item with the key "productList" from localStorage and parse (interpret) it into an object array
+    const taskList = JSON.parse(window.localStorage.getItem("taskList")) || [];
+    // Add the new product to the productList
+    taskList.push(task);
+    // "stringify" the productList back into a String and write it back to LocalStorage
+    window.localStorage.setItem("taskList", JSON.stringify(taskList));
+}
